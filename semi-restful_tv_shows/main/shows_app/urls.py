3f4427ea6fef46f59cea73430be0d34773abd84d
MNeipp <ReamusLQ@gmail.com>
2020-05-31 11:39:52 -0700

    Finished restful-shows assignment. Initial commit

---
+++ semi-restful_tv_shows/main/shows_app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns =[
+    path('', views.index),
+    path('new/', views.new_show),
+    path('<int:show_id>/',views.show_info),
+    path('<int:show_id>/edit/', views.edit_show),
+    path('<int:show_id>/destroy/', views.delete_show),
+    path('new/create_show/', views.create_show),
+    path('<int:show_id>/edit/process/', views.edit_show_process),
+    path('<int:show_id>/destroy/', views.delete_show)
+]
---
+++ semi-restful_tv_shows/main/shows_app/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render, redirect, HttpResponse
+from . models import Show, Network
+
+# Create your views here.
+
+def index(request):
+    context={
+    "shows":Show.objects.all()
+    }
+    return render(request, "index.html", context)
+
+def new_show(request):
+    return render(request, "new_show.html")
+
+def create_show(request):
+    if request.method=="POST":
+        if Network.objects.filter(name__iexact=request.POST['network']).exists():
+            network = Network.objects.get(name__iexact=request.POST['network'])
+        else:
+            network = Network.objects.create(name=request.POST['network'])
+        title = request.POST['title']
+        release_date = request.POST['release_date']
+        desc = request.POST['desc']
+        new_show = Show.objects.create(title=title, network=network ,release_date=release_date, desc=desc) 
+    return redirect(f"/shows/{new_show.id}")
+
+def show_info(request, show_id):
+    context={
+        "show" : Show.objects.get(id=show_id)
+    }
+    return render(request, "show_info.html",context)
+
+def edit_show(request, show_id):
+    context={
+        "show" : Show.objects.get(id=show_id)
+    }
+    return render(request, "edit_show.html", context)
+
+def edit_show_process(request, show_id):
+    if request.method=="POST":
+        edited_show = Show.objects.get(id=show_id)
+        edited_show.title = request.POST['title']
+        edited_show.network.name = request.POST['network']
+        edited_show.release_date = request.POST['release_date']
+        edited_show.desc = request.POST['desc']
+        edited_show.save()
+    return redirect(f"/shows/{show_id}")
+
+def delete_show(request, show_id):
+    delete_show = Show.objects.get(id=show_id)
+    delete_show.delete()
+    return redirect("/shows/")
