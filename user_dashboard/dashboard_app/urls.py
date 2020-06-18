0d1756e41109ff19a704b22a0e8aff2765afa5a9
MNeipp <ReamusLQ@gmail.com>
2020-06-18 17:57:20 -0600

    readied app templates for models

--- user_dashboard/dashboard_app/urls.py
+++ user_dashboard/dashboard_app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from . import views as db_views
 
 urlpatterns =[
-    
+    path('',db_views.home,name="home"),
+    path('dashboard/',db_views.dashboard, name="dashboard"),
 ]
--- user_dashboard/dashboard_app/views.py
+++ user_dashboard/dashboard_app/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def home(request):
+    return render (request, "home.html")
+
+def dashboard(request):
+    return render (request, "dashboard.html")
