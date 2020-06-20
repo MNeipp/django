0b76910da2effa253d0448a073cad8be95c8572d
MNeipp <ReamusLQ@gmail.com>
2020-06-20 12:33:14 -0600

    Slimmed views, added validation managers

--- user_dashboard/dashboard_app/views.py
+++ user_dashboard/dashboard_app/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
 from login_app.models import User
 from django.contrib import messages
-import bcrypt,re
+import bcrypt
 
 # Create your views here.
-EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 def home(request):
     return render (request, "home.html")
@@ -23,31 +22,19 @@ def profile(request):
         return redirect(reverse('register'))
     context={
         "logged_user": User.objects.get(id=request.session['user_id']),
-
     }
     return render(request, "profile.html", context)
 
 def update_info(request):
     if request.method == "GET":
         return redirect(reverse('home'))
-    logged_user = User.objects.get(id=request.session['user_id'])
-    count = 0
-    if not EMAIL_REGEX.match(request.POST['email']):
-        messages.error(request, "Invalid email address.", extra_tags="email")
-        count+=1
-    result_email = User.objects.filter(email__iexact=(request.POST['email']))
-    if len(result_email) > 0 and request.POST['email'] != logged_user.email:
-        messages.error(request, "That email address is already registered.", extra_tags="email")
-        count+=1
-    if len(request.POST['first_name']) < 2:
-        messages.error(request, "First name needs to be at least 2 characters long", extra_tags="first_name")
-        count+=1
-    if len(request.POST['last_name']) < 2:
-        messages.error(request, "Last name needs to be at least 2 characters long", extra_tags="last-name")
-        count+=1
-    if count > 0:
+    errors = User.objects.info_validator(request.POST)
+    if len(errors) > 0:
+        for key,value in errors.items():
+            messages.error(request,value, extra_tags=key)
         return redirect(request.META.get('HTTP_REFERER', 'redirect_if_referer_not_found'))
     else:
+        logged_user = User.objects.get(id=request.session["user_id"])
         logged_user.email = request.POST['email']
         logged_user.first_name = request.POST['first_name']
         logged_user.last_name = request.POST['last_name']
@@ -57,14 +44,13 @@ def update_info(request):
 def update_password(request):
     if request.method == "GET":
         return redirect(reverse('home'))
-    logged_user = User.objects.get(id=request.session['user_id'])
-    if len(request.POST['password']) < 8:
-        messages.error(request, "Your password must be at least 8 characters long", extra_tags="password")
-        return redirect(request.META.get('HTTP_REFERER','redirect_if_referer_not_found'))
-    elif request.POST['password'] != request.POST['confirm_password']:
-        messages.error(request, "Passwords don't match.", extra_tags="confirm")
-        return redirect(request.META.get('HTTP_REFERER','redirect_if_referer_not_found'))
+    errors = User.objects.password_validator(request.POST)
+    if len(errors) > 0:
+        for key,value in errors.items():
+            messages.error(request,value, extra_tags=key)
+        return redirect(request.META.get('HTTP_REFERER', 'redirect_if_referer_not_found'))
     else:
+        logged_user = User.objects.get(id=request.session['user_id'])
         password = request.POST['password']
         pswd_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
         logged_user.password = pswd_hash
@@ -89,6 +75,8 @@ def new_user(request):
         return render(request, "add.html")
 
 def edit_user(request, user_id):
+    if "user_id" not in request.session:
+        return redirect(reverse('home'))
     logged_user = User.objects.get(id=request.session['user_id'])
     if logged_user.user_level != 9:
         return redirect(reverse('dashboard'))
@@ -102,43 +90,29 @@ def edit_user(request, user_id):
 def edit_user_password(request, user_id):
     if request.method == "GET":
         return redirect(reverse('home'))
+    errors = User.objects.password_validator(request.POST)
+    if len(errors) > 0:
+        for key,value in errors.items():
+            messages.error(request,value, extra_tags=key)
+        return redirect(request.META.get('HTTP_REFERER', 'redirect_if_referer_not_found'))
     else:
         edit_user = User.objects.get(id=user_id)
-        if len(request.POST['password']) < 8:
-            messages.error(request, "Your password must be at least 8 characters long", extra_tags="password")
-            return redirect(request.META.get('HTTP_REFERER','redirect_if_referer_not_found'))
-        elif request.POST['password'] != request.POST['confirm_password']:
-            messages.error(request, "Passwords don't match.", extra_tags="confirm")
-            return redirect(request.META.get('HTTP_REFERER','redirect_if_referer_not_found'))
-        else:
-            password = request.POST['password']
-            pswd_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
-            edit_user.password = pswd_hash
-            edit_user.save()
-            return redirect(request.META.get('HTTP_REFERER', 'redirect_if_referer_not_found'))
+        password = request.POST['password']
+        pswd_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+        edit_user.password = pswd_hash
+        edit_user.save()
+        return redirect(request.META.get('HTTP_REFERER', 'redirect_if_referer_not_found'))
 
 def edit_user_info(request, user_id):
     if request.method == "GET":
         return redirect(reverse('home'))
-    edit_user = User.objects.get(id=user_id)
-    count=0
-    if not EMAIL_REGEX.match(request.POST['email']):
-        messages.error(request, "Invalid email address.", extra_tags="email")
-        count+=1
-    result_email = User.objects.filter(email__iexact=(request.POST['email']))
-    if len(result_email) > 0 and request.POST['email'] != edit_user.email:
-        messages.error(request, "That email address is already registered.", extra_tags="email")
-        count+=1
-    if len(request.POST['first_name']) < 2:
-        messages.error(request, "First name needs to be at least 2 characters long", extra_tags="first_name")
-        count+=1
-    if len(request.POST['last_name']) < 2:
-        messages.error(request, "Last name needs to be at least 2 characters long", extra_tags="last-name")
-        count+=1
-    if count > 0:
-        # return redirect(request.META.get('HTTP_REFERER', 'redirect_if_referer_not_found'))
-        return HttpResponse("How the hell did you get here?")
+    errors = User.objects.info_validator(request.POST)
+    if len(errors) > 0:
+        for key,value in errors.items():
+            messages.error(request,value, extra_tags=key)
+        return redirect(request.META.get('HTTP_REFERER', 'redirect_if_referer_not_found'))
     else:
+        edit_user = User.objects.get(id=user_id)
         edit_user.email = request.POST['email']
         edit_user.first_name = request.POST['first_name']
         edit_user.last_name = request.POST['last_name']
--- user_dashboard/login_app/models.py
+++ user_dashboard/login_app/models.py
@@ -20,8 +20,32 @@ class userManager(models.Manager):
         if len(postData['password']) < 8:
             errors['password'] = "Your password must be at least 8 characters long"
         elif postData['password'] != postData['confirm_password']:
-            errors['passwords'] = "Passwords don't match."
+            errors['confirm'] = "Passwords don't match."
         return errors
+
+    def info_validator(self,postData):
+        errors = {}
+        logged_user = User.objects.get(id=postData['user_id'])
+        if len(postData['first_name']) < 2:
+            errors['first_name'] = "First name needs to be at least 2 characters long"
+        if len(postData['last_name']) < 2:
+            errors['last_name'] = "Last name needs to be at least 2 characters long"
+        if not EMAIL_REGEX.match(postData['email']):
+            errors['email'] = "Invalid email address"
+        result_email = User.objects.filter(email__iexact=(postData['email']))
+        if len(result_email) > 0 and postData['email'] != logged_user.email:
+            errors['email'] = "That e-mail address is already registered"
+        return errors
+    
+    def password_validator(self,postData):
+        errors={}
+        if len(postData['password']) < 8:
+            errors['password'] = "Your password must be at least 8 characters long"
+        elif postData['password'] != postData['confirm_password']:
+            errors['confirm'] = "Passwords don't match."
+        return errors
+    
+
 
 class User(models.Model):
     first_name = models.CharField(max_length=255)
